be6a89ec3eaaa943a2a319e42bc855b774107de2
apetreibogdan <70704260+apetreibogdan@users.noreply.github.com>
2022-01-17 16:32:11 +0200

    Centralising the axios requests

---
+++ client/src/app/api/agent.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from "axios";
+
+axios.defaults.baseURL = 'http://localhost:5078/api/';
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const request = {
+    get: (url: string) => axios.get(url).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    delete: (url: string) => axios.delete(url).then(responseBody),
+}
+
+const Catalog = {
+    list: () => request.get('products'),
+    details: (id: number) => request.get(`products/${id}`)
+}
+
+const TestErrors={
+    get400Error:()=> request.get('buggy/not-found'),
+    get401Error:()=> request.get('buggy/unauthorise'),
+    get404Error:()=> request.get('buggy/not-found'),
+    get500Error:()=> request.get('buggy/server-error'),
+    getValidationError:()=> request.get('buggy/validation-error'),
+}
+
+const agent = {
+    Catalog,
+    TestErrors
+}
+export default agent;
+
+
